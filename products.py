1ed58ea5817c8af7b94b4f577c81c307822d04e4
Herbert Kagumba <habukagumba@gmail.com>
2015-05-13 12:11:15 +0300

    Skeleton of the system using webapp2

--- products.py
+++ products.py
@@ -1,15 +1,34 @@
 import webapp2
 
 
+default_page = """
+<html>
+  <body>
+    <form action="/upload" method="post">
+      <div><input type="submit" value="Upload Products"></div>
+    </form>
+  </body>
+</html>
+"""
+
+
 class MainHandler(webapp2.RequestHandler):
 
     """ Receive JSON with product details from HTTP request. """
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Default page')
+        self.response.write(default_page)
+
+
+class ProductHandler(webapp2.RequestHandler):
+
+    """Handle HTTP endpoint to recieve JSON of products. """
+
+    def post(self):
+        self.response.write('Ze outcome')
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/upload', ProductHandler)
 ], debug=True)
